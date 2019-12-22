ecec8ea8eedac28e8dcdce3efa8484b6f5e86801
Ivan Vdovin <ivdovin@aquivalabs.com>
2019-12-23 01:29:31 +0300

    eslint & travis setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!\n");
+const greeting = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
 
-const name = readlineSync.question('May I have your name? ');
-
-console.log(`Hello, ${name}!`);
+export default greeting;
