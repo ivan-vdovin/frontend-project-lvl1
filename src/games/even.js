afbc671fa62ea189d776f0d38e2d00d13cef03c1
Ivan Vdovin <ivdovin@aquivalabs.com>
2020-01-08 16:59:03 +0300

    remove duplicated code

--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,8 @@
 
-import readlineSync from 'readline-sync';
 import getRandomNumber from '../utils';
+import flow from '..';
+
+const task = 'Answer "yes" if the number is even, otherwise answer "no".\n';
 
 const isEven = (number) => number % 2 === 0;
 
@@ -11,36 +13,4 @@ const getGameData = () => {
   return [question, answer];
 };
 
-const levelsCount = 3;
-
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
-
-const playerName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${playerName}!\n`);
-
-const runGame = () => {
-  const iter = (counter) => {
-    if (counter > levelsCount) {
-      console.log(`Congratulations, ${playerName}!`);
-      return;
-    }
-
-    const [question, answer] = getGameData();
-    console.log(`Question: ${question}`);
-    const actual = readlineSync.question('Your answer: ');
-
-    if (actual !== answer) {
-      console.log(`'${actual}' is wrong answer ;(. Correct answer was '${answer}'.`);
-      console.log(`Let's try again, ${playerName}!`);
-      return;
-    }
-
-    console.log('Correct!');
-    iter(counter + 1);
-  };
-
-  iter(1);
-};
-
-export default runGame;
+export default () => flow(task, getGameData);
