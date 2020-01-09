11752ae4327bf1382c79426ed79aeab4b712e091
Ivan Vdovin <ivdovin@aquivalabs.com>
2020-01-10 01:12:34 +0300

    small enhancements

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,32 +4,27 @@ import flow from '..';
 
 const task = 'What number is missing in the progression?\n';
 
-const getProgression = (n) => {
-  const a1 = getRandomNumber(1, 10);
-  const p = getRandomNumber(1, 10);
-  const arr = [a1];
+const progLength = 10;
 
-  for (let i = 1; i < n; i += 1) {
-    arr.push(arr[i - 1] + p);
+const getProgression = (a1, p, length) => {
+  const progression = [a1];
+
+  for (let i = 1; i < length; i += 1) {
+    progression.push(progression[i - 1] + p);
   }
-  return arr;
-};
-
-const getMissedNumProgression = () => {
-  const progression = getProgression(10);
-  const i = getRandomNumber(0, 9);
-
-  const missedNum = progression[i];
-  progression[i] = '..';
-
-  return [progression.join(' '), missedNum];
+  return progression;
 };
 
 const getGameData = () => {
-  const [progression, missedNum] = getMissedNumProgression();
+  const a1 = getRandomNumber(1, 10);
+  const p = getRandomNumber(1, 10);
+  const missedElemIndex = getRandomNumber(1, progLength - 1);
+  const progression = getProgression(a1, p, progLength);
 
-  const question = `${progression}`;
-  const answer = `${missedNum}`;
+  const missedElem = progression.splice(missedElemIndex, 1, '..');
+
+  const question = progression.join(' ');
+  const answer = `${missedElem}`;
 
   return [question, answer];
 };
